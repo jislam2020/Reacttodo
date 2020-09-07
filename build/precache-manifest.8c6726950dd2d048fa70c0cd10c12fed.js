150f098a3540a89855d1e20c88e2e774e1095584
Jahidul Islam <islamjahid316@gmail.com>
2020-09-07 00:33:53 -0400

    Done

---
+++ build/precache-manifest.8c6726950dd2d048fa70c0cd10c12fed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcd438c88db329e87a987a89f4768b91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "accbf4370ad0bbf423f3",
+    "url": "/static/css/main.b8aa9694.chunk.css"
+  },
+  {
+    "revision": "c5f38571c8d30812b65d",
+    "url": "/static/js/2.bd0b50e7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bd0b50e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "accbf4370ad0bbf423f3",
+    "url": "/static/js/main.6215e871.chunk.js"
+  },
+  {
+    "revision": "9944ae3a498fbf61dcd7",
+    "url": "/static/js/runtime-main.25fcc608.js"
+  }
+]);
